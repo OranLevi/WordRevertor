373c5cc0dd4df8defc301f409e4342dd265e674a
Oran Levi <100801409+OranLevi@users.noreply.github.com>
2022-10-31 16:42:49 +0200

    Initial Commit

---
+++ Reverse-Translation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reverse-Translation
+//
+//  Created by Oran Levi on 31/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
